17f632d3b2adb436670132fff4c7db9333161be1
Saurya Raj Pandey <sauryarajp@gmail.com>
2026-01-27 12:27:41 -0500

    Added the login logic and file structuring

---
+++ src/app/(auth)/layout.js
@@ -0,0 +1,9 @@
+  const AuthLayout = ({ children }) => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-green-500 to-blue-800">
+      {children}
+    </div>
+  )
+}
+
+export default AuthLayout
